3c877a6ea814a35e17b3d7895764fbff8f2a0b9b
leoluyi <leo0650@gmail.com>
2017-07-14 01:31:22 +0800

    Show search result in result template

--- run.py
+++ run.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from modules import item
+
 app = Flask(__name__)
 
-
 @app.route("/")
-def hello():
+def index():
     return render_template('base.html')
+
+
+@app.route("/result")
+def result_page():
+    search = request.args.get('search')
+    soup = item.search_soup(search)
+    res = item.find_video_(soup)
+    return render_template('result.html', result=search, all_item=res)
 
 
 if __name__ == '__main__':
